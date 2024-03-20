a4842765da2845b4637074fc635b8df6195fdec8
zawadinjagi <zawadinjagi619@gmail.com>
2024-03-20 09:02:35 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'current least favorite customer';
+  leastFavoriteCustomer = 'new least favorite customer'; 
+}
